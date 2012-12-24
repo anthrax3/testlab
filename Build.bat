28427543be7d1a9cf3268bef497102aed69553d8
Kerry Jiang <kerry-jiang@hotmail.com>
2012-12-24 12:44:36 +0800

    added build script

---
+++ Build.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set fdir=%WINDIR%\Microsoft.NET\Framework64
+
+if not exist %fdir% (
+	set fdir=%WINDIR%\Microsoft.NET\Framework
+)
+
+set msbuild=%fdir%\v4.0.30319\msbuild.exe
+
+%msbuild% testlab.sln /p:Configuration=Debug /t:Clean;Rebuild
+
+
+pause
